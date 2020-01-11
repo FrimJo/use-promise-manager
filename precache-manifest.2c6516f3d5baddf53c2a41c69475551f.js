0a05c61c1d00723b8312626141abd8435fabd509
Fredrik Johansson <fredrik.johansson@thnx.se>
2020-01-11 23:02:51 +0100

    Updates

---
+++ precache-manifest.2c6516f3d5baddf53c2a41c69475551f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e347625dad221a5d69942c0d76720a1",
+    "url": "/use-promise-manager/index.html"
+  },
+  {
+    "revision": "ed876099d4ad0aedbe20",
+    "url": "/use-promise-manager/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "a55720d1e0e1e55aec37",
+    "url": "/use-promise-manager/static/js/2.8df25f9f.chunk.js"
+  },
+  {
+    "revision": "d50198c2b3cb3b9437d9698faa7c978c",
+    "url": "/use-promise-manager/static/js/2.8df25f9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed876099d4ad0aedbe20",
+    "url": "/use-promise-manager/static/js/main.4629473e.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/use-promise-manager/static/js/main.4629473e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6a94610feadd3d1d4ca",
+    "url": "/use-promise-manager/static/js/runtime-main.18d25bb8.js"
+  }
+]);
